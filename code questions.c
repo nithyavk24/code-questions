fe9325c71bb173630bea7dd2f2b2b60e863cff45
nithyavk24 <nithyavkumar2493@gmail.com>
2019-12-08 12:53:33 -0500

    commit 2

--- code questions.c
+++ code questions.c
@@ -17,6 +17,12 @@ if (myFloat == 3.2)
 /* end question 1*/
 
 
+/* float usually creates a double precision variable but when comparing 3.2 which is a single precision float number it looses precision.
+Float is created from a double and hence when declaring a float it will have different value beyond the precision point considered
+myFLoat is a float
+3.2 is of type double
+The complier will convert the float to double and the result of conversion is not the same value. */
+
 
 /* question 2 */
 /* the following code is run on an 8 bit microprocessor*/
@@ -43,3 +49,12 @@ main()
 	}
 }
 /*end question 2*/
+
+
+
+
+
+/* There may be a race condition which may occur in accessing the global variable.
+ A suitably large clock must be selected to give enough time for execution of the main function.
+If a large clock is selected the performance of the Microcontrollers can be hindered 
+If an interrupt is generated during the execution of main function the value may be corrupted.*/
